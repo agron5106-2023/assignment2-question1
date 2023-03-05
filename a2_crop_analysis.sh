fb5b1a0a82218b9bacaabf6f751d2df944c5946b
Steven Wu <stevenhwu@gmail.com>
2023-03-05 16:43:25 +0800

    Commit 4

--- a2_crop_analysis.sh
+++ a2_crop_analysis.sh
@@ -1,7 +1,16 @@
-##### Commit 3 #####
-# Question 1c: What does this commit do?
+##### Commit 4 #####
+# Purpose: Try to find out the largest and smallest number of genes.
+# Question 1d: What's wrong with these commands? How would you proposed to fix this and why?
 
-cat crop_data.csv | cut -d "," -f 1,4-5
+# Sort by number of genes
+cat crop_data.csv | tail -n +2 | cut -d "," -f 6 | sort
+
+# Smallest number_of_genes
+cat crop_data.csv | tail -n +2 | cut -d "," -f 6 | sort | head -n 1
+
+# Largest number_of_genes
+cat crop_data.csv | tail -n +2 | cut -d "," -f 6 | sort | tail -n 1
+
 
 
 
